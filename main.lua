889460c135c45b2130a314935c3eec0365cbeb9f
KylenFR <KylenFR@gmail.com>
2018-04-12 22:19:19 +0200

    first card function and drawing (piocher) function working

--- main.lua
+++ main.lua
@@ -3,11 +3,20 @@
 function love.load()
   require('test_player')
 
-  playerInit()
+  players = {}
+  playerInit(300)
+
+  createCard('test1', 3, 3, 3)
+  createCard('test2', 5, 5, 5)
+
+  width = love.graphics.getWidth()
+  height = love.graphics.getHeight()
+  love.window.setMode(width, height)
 end
 
 
 function love.update(dt)
+  love.window.setTitle(tostring(#players[1].deck) .. '-' .. tostring(#players[1].hand))
 end
 
 
@@ -16,4 +25,7 @@ end
 
 
 function love.keypressed(key, scancode, isrepeat)
+  if key == 'space' then
+    playerDrawCard()
+  end
 end
--- test_player.lua
+++ test_player.lua
@@ -1,22 +1,36 @@
 
 
-function playerInit(deck, lifepoint)
+function playerInit(lifepoint)
   player1 = {}
 
-  player1.deck = deck
+  player1.deck = {}
   player1.hand = {}
   player1.board = {}
   player1.graveyard = {}
 
   player1.life = lifepoint
 
+  table.insert(players, player1)
+end
+
+
+function createCard(name, cost, att, def)
+
+  card = {}
+
+  card.name = name
+  card.cost = cost
+  card.att = att
+  card.def = def
+
+  table.insert(players[1].deck, card)
 end
 
 
 function playerDrawCard()
-  local cardIndex = love.math.random(1, #player1.deck)
-  local cardDrawn = player1.deck[cardIndex]
+  local cardIndex = love.math.random(1, #players[1].deck)
+  local cardDrawn = players[1].deck[cardIndex]
 
-  table.insert(player1.hand, cardDrawn)
-  table.remove(player1.deck, cardIndex)
+  table.insert(players[1].hand, cardDrawn)
+  table.remove(players[1].deck, cardIndex)
 end
